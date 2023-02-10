2905fd56b68da5fc957361a21491e740ca166ce3
anil-r-chauhan <anil.technotery@gmail.com>
2023-02-10 10:42:54 +0530

    first-commit

---
+++ src/components/form/Form.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import React from 'react'
+
+interface formProps {
+    data:Object;
+}
+
+const Form = (props:formProps)=>{
+    return <Typography>hello</Typography>
+}
+
+export default Form
